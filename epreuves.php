09954792c5cdd083ef1137ae13aaacce4f6aa51a
Lionel Lite <liolisena@gmail.com>
2024-06-12 20:46:45 +0100

    modification en cours

---
+++ epreuves.php
@@ -0,0 +1,13 @@
+<article>
+
+	<img src="pics/pdflight.png" class="pdf">
+	<div id="ep">
+		<h2>Devoir</h2>
+		<p><em>Année :</em></p>
+		<a href="">Télécharger</a>
+		<button>Obtenir le corrigé</button>
+	</div>
+
+	
+</article>
+
